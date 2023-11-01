a72cac208e45aa4b5ed47994a71f279884a4d5c6
vondo337 <141491065+ishmam28@users.noreply.github.com>
2023-11-01 20:27:15 +0600

    update

--- loginregsystem.h
+++ loginregsystem.h
@@ -11,4 +11,24 @@ struct User
 void registeraccount(struct User user[], int *usercount, const char username[], const char pin[]);
 int account(const struct User user[], int usercount, const char username[], const char pin[]);
 
+void registeraccount(struct User user[], int *usercount, const char username[], const char pin[])
+{
+  if(*usercount < MAX_USERS)
+  {
+    strcpy(user[*usercount].username, username);
+    strcpy(user[*usercount].pin, pin);
+    (*usercount)++;
+  }
+}
+
+int account(const struct User user[], int usercount, const char username[], const char pin[])
+{
+  for(int i = 0; i < usercount; i++)
+  {
+    if(strcmp(user[i].username, username))
+      return 1; // User found
+  }
+  return -1; // User not found
+}
+
 #endif
