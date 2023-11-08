a927c25cb97df1af3cacc3de20e9fc1970660fec
vondo337 <141491065+ishmam28@users.noreply.github.com>
2023-11-09 04:23:47 +0600

    haw

--- loginregsystem.h
+++ loginregsystem.h
@@ -1,32 +1,28 @@
-#ifndef LOGIN_H
-#define LOGIN_H
+#define MAX_USERS 100
 
-#define MAX_USERS 10
-
-struct User
-{
-  char username[50];
-  int pin[4];    // 4-digit pin
+struct User {
+    char username[50];
+    char password[50];
 };
-
-void registeraccount(struct User user[], int *usercount, const char username[], const int pin[])
-{
-  if(*usercount < MAX_USERS)
-  {
-    strcpy(user[*usercount].username, username);
-    strcpy(user[*usercount].pin, pin);
-    (*usercount)++;
-  }
+void registerUser(struct User users[], int *userCount, const char username[], const char password[]) {
+    if (*userCount < MAX_USERS) {
+        strcpy(users[*userCount].username, username);
+        strcpy(users[*userCount].password, password);
+        (*userCount)++;
+        FILE *file = fopen("users.txt", "a");
+        if (file) {
+            fprintf(file, "%s %s\n", username, password);
+            fclose(file);
+        } else {
+            printf("Failed to write user data to the file.\n");
+        }
+    }
 }
-
-int account(const struct User user[], int usercount, const char username[], const int pin[])
-{
-  for(int i = 0; i < usercount; i++)
-  {
-    if(strcmp(user[i].username, username))
-      return 1; // User found
-  }
-  return -1; // User not found
+int loginUser(struct User users[], int userCount, const char username[], const char password[]) {
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+            return i; // User found, return the index
+        }
+    }
+    return -1; // User not found
 }
-
-#endif
--- main.c
+++ main.c
@@ -20,66 +20,48 @@ int main(void)
   }
   sleep(2);
   system("cls");
-  int choice; 
-  int usercount = 0;
-  struct User user[MAX_USERS];
+  struct User users[MAX_USERS];
+    int userCount = 0;
+    int choice;
+    FILE *file = fopen("users.txt", "r");
+    if (file) {
+        while (fscanf(file, "%s %s", users[userCount].username, users[userCount].password) == 2) {
+            userCount++;
+        }
+        fclose(file);
+    }
 
-  // For loading user data from the text file
-  FILE *file = fopen("users.txt", "r");
-  if(file)
-  {
-    while(fscanf(file, "%s %s", user[usercount].username, user[usercount].pin) == 2)
-    {
-      usercount++;
+    while (1) {
+        printf("                      ---------------------------Welcome to TND Bank ---------------------------\n");
+        printf("1. Register\n2. Login\n3. Quit\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            char username[50], password[50];
+            printf("Enter a username: ");
+            scanf("%s", username);
+            printf("Enter a password: ");
+            scanf("%s", password);
+            registerUser(users, &userCount, username, password);
+        } else if (choice == 2) {
+            char username[50], password[50];
+            printf("Enter your username: ");
+            scanf("%s", username);
+            printf("Enter your password: ");
+            scanf("%s", password);
+            int index = loginUser(users, userCount, username, password);
+            if (index != -1) {
+                printf("Login successful. Welcome, %s!\n", users[index].username);
+            } else {
+                printf("Login failed. Invalid username or password.\n");
+            }
+        } else if (choice == 3) {
+            break;
+        } else {
+            printf("Invalid choice. Please try again.\n");
+        }
     }
-    fclose(file);
-  }
-  
-  while(1)
-  {
-  printf("                      ---------------------------Welcome to TND Bank ---------------------------\n");
-  printf("1. Login\n2. Register\n3. Exit \n");
-  printf("Enter your choice: ");
-  scanf("%d", &choice);
-  if(choice == 1)
-  {
-    char username[50];
-    int pin[4];
-    system("cls");
-    printf("Username: ");
-    scanf("%s", username);
-    printf("PIN CODE: ");
-    scanf("%4d", pin);
-    int index = account(user, usercount, username, pin );
-    if(index != -1)
-    {
-      printf("Login successful. Welcome, %s!\n", user[index].username);
-    }
-    else
-    {
-      printf("Login failed. Invalid username or PIN.\n");
-    }
-  }
-  else if(choice == 2)
-  {
-    char username[50]; 
-    int pin[4];
-    system("cls");
-    printf("Enter your username: ");
-    scanf("%s", username);
-    printf("Enter your 4-digit PIN: ");
-    scanf("%d", pin);
-    registeraccount(user, &usercount, username, pin);
-  }
-  else if(choice == 3)
-  {
-    break;
-  }
-  else
-  {
-    printf("Invalid choice. Please try again.\n");
-  }
-  }
+
 
   
   return 0;
