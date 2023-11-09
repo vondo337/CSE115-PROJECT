baeb81391914a06ba843ae36a6a5b94c4dc3b747
vondo337 <141491065+ishmam28@users.noreply.github.com>
2023-11-09 13:31:07 +0600

    Update loginregsystem.h

--- loginregsystem.h
+++ loginregsystem.h
@@ -1,26 +1,35 @@
 #define MAX_USERS 100
 
-struct User {
+struct User 
+{
     char username[50];
     char password[50];
 };
-void registerUser(struct User users[], int *userCount, const char username[], const char password[]) {
-    if (*userCount < MAX_USERS) {
+void registerUser(struct User users[], int *userCount, const char username[], const char password[]) 
+{
+    if (*userCount < MAX_USERS) 
+    {
         strcpy(users[*userCount].username, username);
         strcpy(users[*userCount].password, password);
         (*userCount)++;
         FILE *file = fopen("users.txt", "a");
-        if (file) {
+        if (file) 
+        {
             fprintf(file, "%s %s\n", username, password);
             fclose(file);
-        } else {
+        } 
+        else 
+        {
             printf("Failed to write user data to the file.\n");
         }
     }
 }
-int loginUser(struct User users[], int userCount, const char username[], const char password[]) {
-    for (int i = 0; i < userCount; i++) {
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+int loginUser(struct User users[], int userCount, const char username[], const char password[]) 
+{
+    for (int i = 0; i < userCount; i++) 
+    {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) 
+        {
             return i; // User found, return the index
         }
     }
